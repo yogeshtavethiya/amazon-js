36e7609f1a4195d4b2cf9b071f8a01ad9e1743b1
yogeshtavethiya <yogesh.it.kumar@gmail.com>
2024-10-09 18:18:18 +0530

    dateset added

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,3 @@
-console.log(products, 'products')
 let productsHTML = ''
 
 
@@ -48,7 +47,7 @@ products.forEach((product)=>{
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button js-add-to-cart button-primary" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
@@ -57,3 +56,28 @@ products.forEach((product)=>{
 })
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML
+
+document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
+  button.addEventListener('click', ()=>{
+    const productId = button.dataset.productId
+
+    let matchingItem
+    
+    cart.forEach((item)=>{
+      if(productId === item.productId){
+        matchingItem = item
+      }
+    })
+
+    if(matchingItem){
+      matchingItem.quantity += 1
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1
+      })
+    }
+
+    console.log(cart)
+  })
+})
